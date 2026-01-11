70ac38b4750f6d8c35097a772638d8835b3b5500
Claipousse <clementconrie@gmail.com>
2026-01-11 12:33:36 +0100

    changed typo for body to zen kaku medium

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "@fontsource-variable/roboto";
 import "@fontsource-variable/roboto-mono";
-import "@fontsource/zen-kaku-gothic-new/700.css"; //only bold
+import "@fontsource/zen-kaku-gothic-new/500.css";
+import "@fontsource/zen-kaku-gothic-new/700.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
